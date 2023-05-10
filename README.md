58cef73b6d875bd4f9efaafb2aea7847472bacba
zhiyiYo <1319158137@qq.com>
2023-05-10 22:36:54 +0800

    第一次提交

---
+++ README.md
@@ -0,0 +1,8 @@
+# Sponsors
+
+<p align="center">
+  <a href="https://cdn.jsdelivr.net/gh/antfu/static/sponsors.svg">
+    <img src='https://cdn.jsdelivr.net/gh/antfu/static/sponsors.svg'/>
+  </a>
+</p>
+
---
+++ sponsor.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig, presets } from 'sponsorkit'
+
+export default defineConfig({
+  // includePrivate: true,
+  providers: ['afdian'],
+  tiers: [
+    {
+      title: '白金之星',
+      preset: presets.xs,
+    },
+    {
+      title: '疯狂钻石',
+      monthlyDollars: 2.5,  //￥16.66
+      // to replace the entire tier rendering
+      // compose: (composer, tierSponsors, config) => {
+      //   composer.addRaw(
+      //     '<-- custom svg -->',
+      //   )
+      // },
+    },
+    {
+      title: '黄金体验',
+      monthlyDollars: 5.6,  //￥36.66
+      preset: presets.medium,
+      // to insert custom elements after the tier block
+      composeAfter: (composer, _tierSponsors, _config) => {
+        composer.addSpan(10)
+      },
+    },
+    {
+      title: '爱之列车',
+      monthlyDollars: 10, //￥66.66
+      preset: presets.large,
+    },
+    {
+      title: '华丽挚爱',
+      monthlyDollars: 25.6,//￥166.66
+      preset: presets.xl,
+    },
+  ],
+})
