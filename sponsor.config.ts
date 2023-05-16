5cd68245e7d99f8c56323f26caa69289141d5368
zhiyiYo <1319158137@qq.com>
2023-05-16 10:12:09 +0800

    更新配置

--- sponsor.config.ts
+++ sponsor.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     {
       title: '疯狂钻石',
       monthlyDollars: 2.5,  //￥16.66
+      preset: presets.small,
       // to replace the entire tier rendering
       // compose: (composer, tierSponsors, config) => {
       //   composer.addRaw(
@@ -21,20 +22,25 @@ export default defineConfig({
     {
       title: '黄金体验',
       monthlyDollars: 5.6,  //￥36.66
-      preset: presets.medium,
+      preset: presets.base,
       // to insert custom elements after the tier block
-      composeAfter: (composer, _tierSponsors, _config) => {
-        composer.addSpan(10)
-      },
+      // composeAfter: (composer, _tierSponsors, _config) => {
+      //   composer.addSpan(10)
+      // },
     },
     {
       title: '爱之列车',
       monthlyDollars: 10, //￥66.66
-      preset: presets.large,
+      preset: presets.medium,
     },
     {
       title: '华丽挚爱',
       monthlyDollars: 25.6,//￥166.66
+      preset: presets.large,
+    },
+    {
+      title: '君の奇跡の愛',
+      monthlyDollars: 41,//￥166.66
       preset: presets.xl,
     },
   ],
